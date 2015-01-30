75cf1fff1b63c224494504cf0be19ff3c6f13f72
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-01-30 10:58:33 +0000

    Scaffold yo angular

---
+++ public/client/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
