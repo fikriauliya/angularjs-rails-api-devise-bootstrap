9af47ac739422d2a5ec5261081469fa4bef66656
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2015-01-31 23:45:10 +0800

    Put APIs under api/ namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/auth'
+  namespace :api, defaults: {format: :json} do
+    mount_devise_token_auth_for 'User', at: '/auth'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
